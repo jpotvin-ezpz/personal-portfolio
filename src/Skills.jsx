8ee3d3cc1027275bdde251f600722ac43ef50e56
Jacob Potvin <jpotvin@protonmail.com>
2021-08-26 11:30:11 -0400

    added gatsby

--- src/Skills.jsx
+++ src/Skills.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 
 const Skills = () => (
   <div className="skills">
@@ -7,27 +7,39 @@ const Skills = () => (
       <ul className="skills-list">
         <li className="skill skill-html">
           <p>HTML</p>
-          <div className="bar"><div className="progress" /></div>
+          <div className="bar">
+            <div className="progress" />
+          </div>
         </li>
         <li className="skill skill-js">
           <p>JavaScript</p>
-          <div className="bar"><div className="progress" /></div>
+          <div className="bar">
+            <div className="progress" />
+          </div>
         </li>
         <li className="skill skill-css">
           <p>CSS</p>
-          <div className="bar"><div className="progress" /></div>
+          <div className="bar">
+            <div className="progress" />
+          </div>
         </li>
         <li className="skill skill-scss">
           <p>Sass</p>
-          <div className="bar"><div className="progress" /></div>
-        </li>
-        <li className="skill skill-bs">
-          <p>Bootstrap</p>
-          <div className="bar"><div className="progress" /></div>
+          <div className="bar">
+            <div className="progress" />
+          </div>
         </li>
         <li className="skill skill-react">
           <p>React</p>
-          <div className="bar"><div className="progress" /></div>
+          <div className="bar">
+            <div className="progress" />
+          </div>
+        </li>
+        <li className="skill skill-gb">
+          <p>Gatsby</p>
+          <div className="bar">
+            <div className="progress" />
+          </div>
         </li>
       </ul>
     </div>
